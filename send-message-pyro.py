f91eb5746482f15a33c8004df46a1f9f1f8f06e8
Victor Ajibade <ajibadevic@gmail.com>
2020-10-25 23:32:14 +0100

    Include session_string

--- send-message-pyro.py
+++ send-message-pyro.py
@@ -3,17 +3,18 @@ import time
 
 api_id = int(environ["API_ID"])
 api_hash = environ["API_HASH"]
-session_string = ""
+session_string = environ["SESSION_STRING"]
 
 app = Client(session_string, api_id, api_hash)
 
 with app:
-    n = 0
-    while n < 4:
-        # Send message with 5 seconds interval
-        print("Waiting...")
-        time.sleep(5)
-        
-        app.send_message("me", f"Heroku test no. {n}...")
-        print("Message sent successfully!")
-        n += 1
+    print("SUCCESS!!!")
+    # n = 0
+    # while n < 4:
+    #     # Send message with 5 seconds interval
+    #     print("Waiting...")
+    #     time.sleep(5)
+    #     
+    #     app.send_message("me", f"Heroku test no. {n}...")
+    #     print("Message sent successfully!")
+    #     n += 1
