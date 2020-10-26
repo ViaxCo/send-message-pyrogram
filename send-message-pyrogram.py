a3fbd8458987a8774a1d58270d157f9f35ea8b3c
Victor Ajibade <ajibadevic@gmail.com>
2020-10-26 08:59:30 +0100

    Use schedule module to make new changes

--- send-message-pyrogram.py
+++ send-message-pyrogram.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 import time
+import schedule
 
 api_id = int(environ["API_ID"])
 api_hash = environ["API_HASH"]
@@ -7,13 +8,43 @@ session_string = environ["SESSION_STRING"]
 
 app = Client(session_string, api_id, api_hash)
 
+# with app:
+#     n = 0
+#     while n < 3:
+#         # Send message with 10 minutes interval
+#         app.send_message("me", f"Heroku test no. {n}...")
+#         
+#         print("Message sent successfully!")
+#         print("Waiting...")
+#         time.sleep(600)
+#         n += 1
+
+###################################################################
+
+n = 0
+
+
+def job():
+    global n
+    app.send_message("me", f"Heroku test no. {n}...")
+    print("Message sent successfully!")
+    print("Waiting...")
+    n += 1
+
+
+# schedule.every().day.at("10:30").do(job)
+# schedule.every(5).seconds.do(job)
+schedule.every(30).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
+
 with app:
-    n = 0
-    while n < 3:
-        # Send message with 10 minutes interval
-        app.send_message("me", f"Heroku test no. {n}...")
-        
-        print("Message sent successfully!")
-        print("Waiting...")
-        time.sleep(600)
-        n += 1
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        if n == 18:
+            app.send_message(
+                "me", "This ran for 9 hours with a 30 minute interval and has now come to an end.")
+            break
