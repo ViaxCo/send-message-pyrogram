d93715c024aae5d28c9e1cadc5bb98fef428da95
Victor Ajibade <ajibadevic@gmail.com>
2020-10-28 16:24:36 +0100

    Include documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# send-message-pyrogram
+
+A python script used to send messages to users using the [Pyrogram](https://github.com/pyrogram/pyrogram) framework for Telegram.
--- send-message-pyrogram.py
+++ send-message-pyrogram.py
@@ -1,6 +1,9 @@
+# A script to use pyrogram module to send messages to telegram users periodically using pyrogram
+# The PyPi schedule module can be used to achieve this or the inbuilt time module
 import schedule
 import time
 from pyrogram import Client
+# For environment variables
 from os import environ
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,33 +14,33 @@ session_string = environ["SESSION_STRING"]
 
 app = Client(session_string, api_id, api_hash)
 
+# USING THE INBUILT TIME MODULE
 # with app:
 #     n = 0
 #     while n < 3:
 #         # Send message with 10 minutes interval
-#         app.send_message("me", f"Heroku test no. {n}...")
-#         
+#         app.send_message("me", f"Test no. {n}...")
 #         print("Message sent successfully!")
 #         print("Waiting...")
 #         time.sleep(600)
 #         n += 1
 
-###################################################################
 
+# USING THE SCHEDULE MODULE
 n = 0
 
 
 def job():
     global n
-    app.send_message("me", f"Heroku test no. {n}...")
+    app.send_message("me", f"Test no. {n}...")
     print("Message sent successfully!")
     print("Waiting...")
     n += 1
 
 
 # schedule.every().day.at("10:30").do(job)
-schedule.every(5).seconds.do(job)
-# schedule.every(30).minutes.do(job)
+# schedule.every(5).seconds.do(job)
+schedule.every(30).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().monday.do(job)
 # schedule.every().wednesday.at("13:15").do(job)
